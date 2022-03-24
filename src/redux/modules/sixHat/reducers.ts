2bb3ac18e3be0cef1ac26ba2f3aedb2c9da6fc18
dongyun <han10091009@naver.com>
2022-03-25 03:08:53 +0900

    ddd

--- src/redux/modules/sixHat/reducers.ts
+++ src/redux/modules/sixHat/reducers.ts
@@ -14,9 +14,12 @@ const initialState: SixHatState = {
   nickname: null,
   isAdmin: false,
   isSubmit: false,
-  chatHistory: [],
+  // chatHistory: [],
   subject: undefined,
-  userList: [],
+  userList: {
+    nickname: null,
+    hat: null
+  }
 };
 
 //createReducer로 reducer 생성.
@@ -34,27 +37,27 @@ export const sixHatReducer = createReducer(initialState, builder => {
     .addCase(changeIsSubmitState, (state, action) => {
       state.isSubmit = action.payload;
     })
-    .addCase(getMessages, (state, action) => {
-      if (state.chatHistory) {
-        state.chatHistory = [action.payload, ...state.chatHistory];
-      }
-    })
-    .addCase(getUserHatInfo, (state, action) => {
-      const { nickname, hat } = action.payload;
-      //닉네임만 뽑아낸 리스트
-      const nicknameList = state.userList?.map(item => item.nickname);
+    // .addCase(getMessages, (state, action) => {
+    //   if (state.chatHistory) {
+    //     state.chatHistory = [action.payload, ...state.chatHistory];
+    //   }
+    // })
+    // .addCase(getUserHatInfo, (state, action) => {
+    //   const { nickname, hat } = action.payload;
+    //   //닉네임만 뽑아낸 리스트
+    //   const nicknameList = state.userList?.map(item => item.nickname);
 
-      //그 닉네임 리스트중에서 지금 들어온 유저의 이름이 있는지 확인
-      if (nicknameList?.includes(nickname)) {
-        //유저의 이름이 있으면, 해당 유저의 인덱스를 찾기
-        const idx = nicknameList.indexOf(nickname);
-        //userlist에서 해당 인덱스에 있는 요소의 모자 값에 지금 들어온 모자값을 넣어주기
-        state.userList[idx].hat = hat;
-      } else {
-        //유저의 이름이 없다면, 해당 정보를 추가해주기.
-        state.userList?.push(action.payload);
-      }
-    });
+    //   //그 닉네임 리스트중에서 지금 들어온 유저의 이름이 있는지 확인
+    //   if (nicknameList?.includes(nickname)) {
+    //     //유저의 이름이 있으면, 해당 유저의 인덱스를 찾기
+    //     const idx = nicknameList.indexOf(nickname);
+    //     //userlist에서 해당 인덱스에 있는 요소의 모자 값에 지금 들어온 모자값을 넣어주기
+    //     state.userList[idx].hat = hat;
+    //   } else {
+    //     //유저의 이름이 없다면, 해당 정보를 추가해주기.
+    //     state.userList?.push(action.payload);
+    //   }
+    // });
 });
 
 //객체가 담겨 있는 배열안에서, 특정 값을 가지고 있는지 확인하기.
